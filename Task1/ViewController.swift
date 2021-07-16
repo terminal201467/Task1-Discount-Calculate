6bab10c1fdbfe97019c9d7e7e2f841c567d63656
MJ <terminal201467@gmail.com>
2021-07-16 15:44:36 +0800

    Initial Commit

---
+++ Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Jhen Mu on 2021/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
